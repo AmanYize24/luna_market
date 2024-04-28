60f2732dcd96f2d34f82ae1a8555c53591a593a5
AmanYize24 <amanyahweh@gmail.com>
2024-04-28 01:43:43 -0700

    admin exp 2

--- lib/admin/admin_storage.dart
+++ lib/admin/admin_storage.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 
 import 'package:file_picker/file_picker.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 
 Future<String> chooseFile() async {
   FilePickerResult? result = await FilePicker.platform.pickFiles();
@@ -12,4 +13,57 @@ Future<String> chooseFile() async {
   } else {
     return "";
   }
+}
+
+Future<List> getItems({required String name}) async {
+  final storageRef = FirebaseStorage.instance.ref();
+
+  var listRef = storageRef.child(name);
+  final listOfProducts = [];
+  final items = await listRef.listAll().then((value) => value.items);
+  final List listPath = [];
+  for (int i = 0; i < items.length; i++) {
+    listPath.add([items[i].fullPath, items[i].fullPath]);
+  }
+  for (int i = 0; i < listPath.length; i++) {
+    final itemImgUrl = await storageRef.child(listPath[i][0]).getDownloadURL();
+
+    final itemPath = storageRef.child(listPath[i][1]);
+    final itemName = itemPath.fullPath.split('&')[0].split('/')[1];
+    final itemPrice = itemPath.fullPath.split('&')[1];
+    final itemDescription = itemPath.fullPath.split('&')[2];
+    listOfProducts.add({
+      "url": itemImgUrl,
+      "name": itemName,
+      "price": itemPrice,
+      "description": itemDescription
+    });
+  }
+
+  return listOfProducts;
+}
+
+Future<bool> deleteProduct(
+    {required String refName,
+    required String name,
+    required String price,
+    required String ext,
+    required String description}) async {
+  bool checkDelete = false;
+  final storageRef = FirebaseStorage.instance.ref();
+  try {
+    final item = storageRef.child("$refName/$name&$price&$description&.$ext");
+
+    print("item found at $item");
+    await item.delete();
+    checkDelete = true;
+  } catch (e) {
+    print(e);
+    checkDelete = false;
+  }
+  return checkDelete;
+}
+
+Future<String> pickUploadFile() async {
+  return " ";
 }
--- lib/admin/screens/products.dart
+++ lib/admin/screens/products.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:luna_market/admin/admin_storage.dart';
+import 'package:luna_market/widgets/box.dart';
 
 class Products extends StatefulWidget {
   final String title;
@@ -15,41 +18,190 @@ class Products extends StatefulWidget {
 }
 
 class _ProductsState extends State<Products> {
+  bool loading = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: SizedBox(
-        width: double.infinity,
-        height: double.infinity,
-        child: FutureBuilder(
-            future: widget.products(),
-            builder: (context, snapshot) {
-              if (snapshot.hasData) {
-                return ListView.builder(
-                    itemCount: widget.productList.length,
-                    itemBuilder: (context, index) => Container(
-                          width: 300,
-                          height: 200,
-                          decoration: BoxDecoration(
-                              border: Border.all(color: Colors.black)),
-                          child: const Column(
-                            children: [
-                              SizedBox(width: 100, height: 100, child: Text(''))
-                            ],
-                          ),
-                        ));
-              } else if (snapshot.connectionState == ConnectionState.waiting) {
-                return const Center(
-                  child: CircularProgressIndicator(),
-                );
-              } else {
-                return const Text("Error");
-              }
-            }),
-      ),
-    );
+        appBar: AppBar(
+          title: Text(widget.title),
+        ),
+        body: SizedBox(
+          width: double.infinity,
+          height: double.infinity,
+          child: ListView(
+            children: [
+              SizedBox(
+                width: double.infinity,
+                height: 620,
+                child: FutureBuilder(
+                    future: getItems(name: widget.title),
+                    builder: (context, snapshot) {
+                      if (snapshot.hasData) {
+                        return ListView.builder(
+                            itemCount: snapshot.data!.length,
+                            itemBuilder: (context, index) => SizedBox(
+                                  width: double.infinity,
+                                  height: 150,
+                                  child: ListView(
+                                    scrollDirection: Axis.horizontal,
+                                    children: [
+                                      Align(
+                                        alignment: Alignment.topLeft,
+                                        child: Container(
+                                          width: 360,
+                                          height: 150,
+                                          margin: EdgeInsets.symmetric(
+                                              vertical: 10, horizontal: 5),
+                                          padding: EdgeInsets.symmetric(
+                                              vertical: 10),
+                                          decoration: BoxDecoration(
+                                              border: Border.all(
+                                                  color: Colors.black)),
+                                          child: ListView(
+                                            scrollDirection: Axis.horizontal,
+                                            children: [
+                                              Align(
+                                                alignment: Alignment.topLeft,
+                                                child: SizedBox(
+                                                    width: 150,
+                                                    height: 150,
+                                                    child: Image(
+                                                      fit: BoxFit.contain,
+                                                      image: NetworkImage(
+                                                          '${snapshot.data![index]["url"]}'),
+                                                    )),
+                                              ),
+                                              box(10, 0),
+                                              Column(
+                                                children: [
+                                                  Align(
+                                                    alignment:
+                                                        Alignment.topLeft,
+                                                    child: SizedBox(
+                                                        width: 200,
+                                                        height: 25,
+                                                        child: Text(
+                                                            style: GoogleFonts.inter(
+                                                                fontWeight:
+                                                                    FontWeight
+                                                                        .w600),
+                                                            '${snapshot.data![index]["name"]}')),
+                                                  ),
+                                                  Align(
+                                                    alignment:
+                                                        Alignment.topLeft,
+                                                    child: SizedBox(
+                                                        width: 200,
+                                                        height: 25,
+                                                        child: Text(
+                                                            style: GoogleFonts
+                                                                .inter(
+                                                              fontWeight:
+                                                                  FontWeight
+                                                                      .w600,
+                                                              color:
+                                                                  Colors.grey,
+                                                            ),
+                                                            '${snapshot.data![index]["price"]}')),
+                                                  ),
+                                                  Align(
+                                                    alignment:
+                                                        Alignment.topLeft,
+                                                    child: SizedBox(
+                                                        width: 220,
+                                                        height: 50,
+                                                        child: Text(
+                                                            style: GoogleFonts
+                                                                .inter(
+                                                              fontWeight:
+                                                                  FontWeight
+                                                                      .w600,
+                                                              color:
+                                                                  Colors.grey,
+                                                              fontSize: 11,
+                                                            ),
+                                                            '${snapshot.data![index]["description"]}')),
+                                                  ),
+                                                ],
+                                              ),
+                                            ],
+                                          ),
+                                        ),
+                                      ),
+                                      Align(
+                                        alignment: Alignment.center,
+                                        child: Container(
+                                          width: 40,
+                                          decoration: BoxDecoration(
+                                              border: Border.all(
+                                                  color: Colors.black)),
+                                          height: 130,
+                                          child: IconButton(
+                                              onPressed: () async {
+                                                setState(() {
+                                                  loading = true;
+                                                });
+                                                final List arr = snapshot
+                                                    .data![index]["url"]
+                                                    .split(".");
+                                                final String ext =
+                                                    arr[arr.length - 1]
+                                                        .split("?")[0];
+                                                await deleteProduct(
+                                                    refName: widget.title,
+                                                    name: snapshot.data![index]
+                                                        ["name"],
+                                                    price: snapshot.data![index]
+                                                        ["price"],
+                                                    ext: ext,
+                                                    description:
+                                                        snapshot.data![index]
+                                                            ["description"]);
+                                                setState(() {
+                                                  loading = false;
+                                                });
+                                              },
+                                              icon: loading
+                                                  ? const Center(
+                                                      child:
+                                                          CircularProgressIndicator(),
+                                                    )
+                                                  : const Icon(Icons.delete)),
+                                        ),
+                                      )
+                                    ],
+                                  ),
+                                ));
+                      } else if (snapshot.connectionState ==
+                          ConnectionState.waiting) {
+                        return const Center(
+                          child: CircularProgressIndicator(),
+                        );
+                      } else {
+                        return const Text("Error");
+                      }
+                    }),
+              ),
+              box(0, 25),
+              Align(
+                alignment: Alignment.center,
+                child: SizedBox(
+                  width: 300,
+                  height: 50,
+                  child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.purpleAccent),
+                      onPressed: () {},
+                      child: Text(
+                          style: GoogleFonts.inter(
+                              color: Colors.white,
+                              fontWeight: FontWeight.w700,
+                              fontSize: 18),
+                          "Add Product")),
+                ),
+              )
+            ],
+          ),
+        ));
   }
 }
